39c7ff577e2a37afa4326a314674ea9471c86983
l0450 <61821254+l0450@users.noreply.github.com>
2025-08-20 12:06:11 +0200

    Update TemperatureConverter.java

--- JavaPrograms/src/TemperatureConverter.java
+++ JavaPrograms/src/TemperatureConverter.java
@@ -2,7 +2,15 @@
 public class TemperatureConverter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Declare two variables of type int
+		int f, c;
+		// Assign 21 to c
+		c = 21;
+		// Assign the result of the formula to f
+		f = c*9/5+32;
+		// Print out the results
+		System.out.println(c + " degrees Centigrade = " 
+				+ f + " degrees Fahrenheit");
 
 	}
 
